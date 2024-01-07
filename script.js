d8e35d02acf3a756a2e7728f94c261409e473005
Jyotishko Bhattacharjee <90274384+jyotishkob@users.noreply.github.com>
2024-01-07 18:32:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your code here
+let evaluatedText = document.getElementById("evaluatedText")
+evaluatedText.addEventListener("input", () => {
+	let count = (evaluatedText.value).length;
+	document.getElementById("letterCount").textContent = `Total characters: ${count}`;
+})
